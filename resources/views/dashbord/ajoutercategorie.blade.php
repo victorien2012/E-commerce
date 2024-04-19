eae38b65cf83778cc45bf1eeb469e888ce4ddba1
DESKTOP-797\AGENT DGI <koffikouassivictorien@gmail.com>
2024-04-19 13:56:06 +0000

    correction du formulaaire ajouter catégorie

--- resources/views/dashbord/ajoutercategorie.blade.php
+++ resources/views/dashbord/ajoutercategorie.blade.php
@@ -12,17 +12,16 @@
         <div class="col-lg-12">
             <div class="card">
               <div class="card-body">
-                <h4 class="card-title">Ajouter Catégories</h4>
-                <form class="cmxform" id="commentForm" method="get" action="#">
-                  {!!Form::open(['action' =>'CategoryController@sauvercategorie', 'method'=> 'POST', 'class' => 'cmxform', 'id'=>'commentForm'])!!}
-                  {{ csrf_field() }}
-                    <div class="form-group">
-                      {{Form::label('', 'Nom de la Catégorie', ['For'=>'cname'])}}
-                      {{Form::text('category_name', '', ['class'=>'form-control', 'id'=>'cname'])}}
-                    </div>
-                    {!!Form::submit('Ajouter', 'class'=>'btn btn-primary')!!}
-                    {!!Form::close()!!}
-                </form>
+                <h4 class="card-title">Ajouter Catégorie</h4>
+                {!! Form::open(['action' =>'CategoryController@sauvercategorie', 'method'=> 'POST', 'class' => 'cmxform', 'id'=>'commentForm']) !!}
+                {{ csrf_field() }}
+                <div class="form-group">
+                  {{ Form::label('', 'Nom de la Catégorie', ['for'=>'cname']) }}
+                  {{ Form::text('category_name', '', ['class'=>'form-control', 'id'=>'cname']) }}
+                </div>
+                {!! Form::submit('Ajouter', ['class'=>'btn btn-primary']) !!} <!-- Correction ici -->
+              {!! Form::close() !!}
+              
               </div>
             </div>
           </div>
